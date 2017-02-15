f07ff4c7ac0c01eb823a0411d33eecbb8722ddcf
Didi Pepple <didi.pepple@gmail.com>
2017-02-15 09:55:44 +0100

    an example of a Service

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,9 +1,12 @@
 angular.module("ToDoApp", [])
-    .controller("MainctrlController", function ($scope) {
+    .controller("MainctrlController", function ($scope, dataService) {
         $scope.hello = function () {
             console.log("This prints out a message, and is a function that is triggered when its injected into a html element. :)");
         };
         $scope.todo = "";
+
+        $scope.consolelogs = dataService.helloconsole;
+
 
         $scope.todos = [
             {"name" : "clean the house"},
@@ -13,4 +16,10 @@ angular.module("ToDoApp", [])
             {"name" : "go shop for clothes"},
             {"name" : "take a break"}
         ]
+    })
+    .service("dataService", function () {
+
+        this.helloconsole = function () {
+            console.log("This is an example of a Service in action!");
+        }
     });
