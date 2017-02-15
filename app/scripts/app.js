1d3f92dc24f68b56cb16153388dfdaabb5477713
Didi Pepple <didi.pepple@gmail.com>
2017-02-16 00:15:10 +0100

    created the addTodo function, deleteTodo, function and saveTodo function with delete and save corresponding services

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,25 +1,46 @@
 angular.module("ToDoApp", [])
     .controller("MainctrlController", function ($scope, dataService) {
-        $scope.hello = function () {
-            console.log("This prints out a message, and is a function that is triggered when its injected into a html element. :)");
+        $scope.addTodo = function () {
+            var todo = { name : "This is a new List Item to add"};
+            $scope.todos.push(todo);
         };
-        $scope.todo = "";
 
         $scope.consolelogs = dataService.helloconsole;
 
+       dataService.getTodos((function (response) {
+           $scope.todos = response.data;
+       }));
 
-        $scope.todos = [
-            {"name" : "clean the house"},
-            {"name" : "wash the car"},
-            {"name" : "go and buy chaw"},
-            {"name" : "buy toiletries"},
-            {"name" : "go shop for clothes"},
-            {"name" : "take a break"}
-        ]
+        $scope.deleteTodo = function(todo, $index) {
+            dataService.deleteTodo(todo);
+            $scope.todos.splice($index, 1);
+        };
+
+        $scope.saveTodo = function (todo) {
+            dataService.saveTodo(todo);
+        }
+
     })
-    .service("dataService", function () {
+    .service("dataService", function ($http) {
 
         this.helloconsole = function () {
             console.log("This is an example of a Service in action!");
+        };
+
+        this.getTodos = function(callback){
+            $http.get('data/todos.json')
+                .then(callback);
+        };
+
+        this.deleteTodo = function (todo) {
+            console.log("The " + todo.name + " has been deleted successfuly!");
+
+            //other logic depending on call from the server.
+        };
+
+        this.saveTodo = function (todo) {
+            console.log("The " + todo.name + " has been saved successfuly!");
+
+            //other logic depending on call from the server.
         }
     });
