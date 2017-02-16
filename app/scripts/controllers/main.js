cbd3c6ecb73d371d30c3ba95a5e856b6e38fcb8e
Didi Pepple <didi.pepple@gmail.com>
2017-02-16 15:07:52 +0100

    restructured project files

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Diimiegbo on 16/02/2017.
+ */
+
+'use strict';
+
+
+angular.module('ToDoApp')
+
+    .controller('MainctrlController', function ($scope, dataService) {
+        $scope.addTodo = function () {
+            var todo = { name : "This is a new List Item to add"};
+            $scope.todos.push(todo);
+        };
+
+        $scope.consolelogs = dataService.helloconsole;
+
+        dataService.getTodos((function (response) {
+            $scope.todos = response.data;
+        }));
+
+        $scope.deleteTodo = function(todo, $index) {
+            dataService.deleteTodo(todo);
+            $scope.todos.splice($index, 1);
+        };
+
+        $scope.saveTodo = function (todo) {
+            dataService.saveTodo(todo);
+        }
+
+    });
