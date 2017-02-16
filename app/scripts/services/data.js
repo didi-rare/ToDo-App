797204e4dd5d7f14360180bfe45b3fd7cb14dd80
Didi Pepple <didi.pepple@gmail.com>
2017-02-16 15:07:52 +0100

    restructured project files

---
+++ app/scripts/services/data.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Diimiegbo on 16/02/2017.
+ */
+
+'use strict';
+
+
+angular.module('ToDoApp')
+
+    .service('dataService', function ($http) {
+
+        this.helloconsole = function () {
+            console.log("This is an example of a Service in action!");
+        };
+
+        this.getTodos = function(callback){
+            $http.get('data/todos.json')
+                .then(callback);
+        };
+
+        this.deleteTodo = function (todo) {
+            console.log("The " + todo.name + " has been deleted successfuly!");
+
+            //other logic depending on call from the server.
+        };
+
+        this.saveTodo = function (todo) {
+            console.log("The " + todo.name + " has been saved successfuly!");
+
+            //other logic depending on call from the server.
+        }
+    });
